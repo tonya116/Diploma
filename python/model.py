51d48843411f425043245031346747eb91137a97
tonya116 <idanya2003@gmail.com>
2024-11-09 20:46:36 +0300

    Replace some fields from main to model

--- python/model.py
+++ python/model.py
@@ -1,8 +1,6 @@
 ﻿import json
 import math
-
-# from dearpygui import dearpygui as dpg
-
+from dearpygui import dearpygui as dpg
 
 class Model:
     def __init__(self):
@@ -16,6 +14,13 @@ class Model:
         self.materials = {}
         self.loads = []
         self.boundary_conditions = []
+        self.view = dpg.create_fps_matrix([0, 0, 50], 0.0, 0.0)
+        self.proj = dpg.create_perspective_matrix(math.pi * 45.0 / 180.0, 1.0, 0.1, 100)
+        self.model_matrix = (
+            dpg.create_rotation_matrix(math.radians(self.x_rot), [1, 0, 0])
+            * dpg.create_rotation_matrix(math.radians(self.y_rot), [0, 1, 0])
+            * dpg.create_rotation_matrix(math.radians(self.z_rot), [0, 0, 1])
+        )
 
     def rotate_x(self, angle):
         self.x_rot += angle
@@ -29,8 +34,9 @@ class Model:
     def load_model(self, filename):
         with open(filename, "r", encoding="utf-8-sig") as file:
             self.data = json.load(file)
+        self.__parse_model()
 
-    def parse_model(self):
+    def __parse_model(self):
         self.nodes = self.data.get("nodes")
         self.elements = self.data.get("elements")
         self.materials = self.data.get("materials")
@@ -45,3 +51,12 @@ class Model:
             self.boundary_conditions,
             sep="\n",
         )
+
+    def update(self):
+        self.model_matrix = (
+            dpg.create_rotation_matrix(math.radians(self.x_rot), [1, 0, 0])
+            * dpg.create_rotation_matrix(math.radians(self.y_rot), [0, 1, 0])
+            * dpg.create_rotation_matrix(math.radians(self.z_rot), [0, 0, 1])
+        )
+
+        dpg.apply_transform("cube", self.proj * self.view * self.model_matrix)
