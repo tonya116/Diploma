62ef896492e84a1ff7cd50d34c57e95bbb4a6d3a
Danya Novikov <dnovikov@softdev.spb.ru>
2025-03-29 04:51:28 +0300

    Try to split logic

--- include/calculations.h
+++ include/calculations.h
@@ -1,6 +1,104 @@
 ﻿#pragma once
 #include <cstdio>
 #include <cmath>
-extern "C" void print_info(double arr[], size_t size);
+#include <iostream>
+#include "../include/matrix.h"
+
+extern "C" double integral(double (*func)(double), double a, double b, double step = 0.1);
+
+// // Структура для описания нагрузки
+// struct Load {
+//     double position; // Позиция приложения нагрузки (м)
+//     double magnitude; // Величина нагрузки (Н)
+//     bool is_distributed; // Истина, если нагрузка распределённая
+//     double length; // Длина распределённой нагрузки (если применимо)
+// };
+
+// // Структура для описания балки
+// struct Beam {
+//     double length; // Длина балки (м)
+//     double EI; // Жёсткость балки (Н*м^2)
+//     std::vector<Load> loads; // Нагрузки
+// };
+
+// // Структура для описания реакций
+// struct Reaction {
+//     double left; // Реакция левой опоры (Н)
+//     double right; // Реакция правой опоры (Н)
+// };
 
 
+
+// // Расчёт реакций опор
+// Reaction calculateReactions(const Beam& beam);
+// // Расчёт прогиба в точке x
+// double calculateDeflection(const Beam& beam, const Reaction& reactions, double x);
+
+
+
+
+// Узел системы
+struct Node {
+    double x, y; // Координаты узла
+    int id;      // Идентификатор узла
+};
+
+// Сила
+struct Force {
+    int node_id;  // ID узла, к которому приложена сила
+    double fx, fy; // Компоненты силы
+};
+
+// Момент силы
+struct Moment {
+    int node_id;   // ID узла, к которому приложен момент
+    double value;  // Величина момента (в Н*м)
+};
+
+// Распределенная сила
+struct DistributedForce {
+    int element_id; // ID элемента, на который действует сила
+    double q;       // Интенсивность нагрузки (Н/м)
+    double start;   // Начальная координата действия нагрузки (относительно длины элемента)
+    double end;     // Конечная координата действия нагрузки (относительно длины элемента)
+};
+
+
+// Элемент системы
+struct Element {
+    int start_node_id, end_node_id; // Узлы, соединенные элементом
+    double stiffness;              // Жесткость элемента
+};
+
+// Структура для описания реакций
+struct Reaction {
+    double left; // Реакция левой опоры (Н)
+    double right; // Реакция правой опоры (Н)
+};
+
+// Статически неопределимая система
+class StaticSystem {
+private:
+    std::vector<Node> nodes;
+    std::vector<Element> elements;
+    std::vector<Force> forces;
+    std::vector<Moment> moments;
+    std::vector<DistributedForce> distributed_forces;
+
+public:
+    void addDistributedForce(int element_id, double q, double start, double end);
+    // Добавить узел
+    void addNode(double x, double y);
+
+    // Добавить элемент
+    void addElement(int start_node_id, int end_node_id, double stiffness);
+
+    // Добавить силу
+    void addForce(int node_id, double fx, double fy) ;
+
+    // Добавить момент
+    void addMoment(int node_id, double value) ;
+
+    // Рассчитать систему
+    void calculate() ;
+};
--- src/calculations.cpp
+++ src/calculations.cpp
@@ -1,11 +1,209 @@
 ﻿#include "../include/calculations.h"
-// #include "../include/matrix.h"
+#include "matrix.h"
 #include <cmath>
 #include <cstdio>
+#include <iostream>
 
-void print_info(double arr[], size_t size) {
-  for (size_t i = 0; i < size; ++i)
-    printf("%f\n", arr[i]);
+double integral( double (*func)(double), double a, double b, double step){
+  double res = 0;
+  for(double i = a; i < b; i+= step){
+    std::cout << res << std::endl;
+    res += func(i) * step;
+  }
+  return res;
 }
 
+// // Расчёт реакций опор
+// Reaction calculateReactions(const Beam& beam) {
+//     double total_force = 0.0;
+//     double total_moment = 0.0;
 
+//     for (const auto& load : beam.loads) {
+//         if (load.is_distributed) {
+//             // Считаем эквивалентную силу и её точку приложения
+//             double equivalent_force = load.magnitude * load.length;
+//             double equivalent_position = load.position + load.length / 2.0;
+
+//             total_force += equivalent_force;
+//             total_moment += equivalent_force * equivalent_position;
+//         } else {
+//             total_force += load.magnitude;
+//             total_moment += load.magnitude * load.position;
+//         }
+//     }
+
+//     double right_reaction = total_moment / beam.length;
+//     double left_reaction = total_force - right_reaction;
+
+//     return {left_reaction, right_reaction};
+// }
+
+// // Расчёт прогиба в точке x
+// double calculateDeflection(const Beam& beam, const Reaction& reactions, double x) {
+//     double v = 0.0;
+
+//     for (const auto& load : beam.loads) {
+//         if (x < load.position) {
+//             continue; // Прогиб из-за нагрузки ещё не влияет
+//         }
+
+//         if (load.is_distributed) {
+//             double start = load.position;
+//             double end = load.position + load.length;
+
+//             if (x <= end) {
+//                 // Прогиб из-за распределённой нагрузки в пределах её действия
+//                 double q = load.magnitude; // Интенсивность распределённой нагрузки
+//                 double a = x - start;
+//                 v += (q * a * a * a) / (6 * beam.EI);
+//             } else {
+//                 // Прогиб из-за полной распределённой нагрузки
+//                 double q = load.magnitude;
+//                 double L = load.length;
+//                 double a = x - start;
+//                 v += (q * L * L * L) / (6 * beam.EI);
+//             }
+//         } else {
+//             // Прогиб из-за точечной нагрузки
+//             double a = load.position;
+//             v += (load.magnitude * std::pow(x - a, 3)) / (6 * beam.EI);
+//         }
+//     }
+
+//     // Прогиб от реакций
+//     v -= (reactions.left * std::pow(x, 3)) / (6 * beam.EI);
+//     v += (reactions.right * std::pow(beam.length - x, 3)) / (6 * beam.EI);
+
+//     return v;
+// }
+
+void StaticSystem::addDistributedForce(int element_id, double q, double start, double end) {
+        distributed_forces.push_back({element_id, q, start, end});
+    }
+
+// Добавить узел
+void StaticSystem::addNode(double x, double y) {
+    nodes.push_back({x, y, static_cast<int>(nodes.size())});
+}
+
+// Добавить элемент
+void StaticSystem::addElement(int start_node_id, int end_node_id, double stiffness) {
+    elements.push_back({start_node_id, end_node_id, stiffness});
+}
+
+// Добавить силу
+void StaticSystem::addForce(int node_id, double fx, double fy) {
+    forces.push_back({node_id, fx, fy});
+}
+
+// Добавить момент
+void StaticSystem::addMoment(int node_id, double value) {
+    moments.push_back({node_id, value});
+}
+
+// Рассчитать систему
+void StaticSystem::calculate() {
+  int n = nodes.size();
+  int eq_count = 3 * n;// Уравнения: 2 силы (x, y) + 1 момент на каждый узел
+  Matrix stiffness_matrix(eq_count, eq_count);
+  std::vector<double> load_vector(eq_count, 0.0);
+
+  // Заполнение вектора нагрузок от сосредоточенных сил
+  for (const auto& force : forces) {
+      int index_x = 3 * force.node_id;
+      int index_y = 3 * force.node_id + 1;
+      load_vector[index_x] += force.fx;
+      load_vector[index_y] += force.fy;
+  }
+
+  // Учет распределенных сил
+  for (const auto& df : distributed_forces) {
+      const Element& element = elements[df.element_id];
+      const Node& start_node = nodes[element.start_node_id];
+      const Node& end_node = nodes[element.end_node_id];
+
+      // Длина элемента
+      double L = std::sqrt(std::pow(end_node.x - start_node.x, 2) +
+                            std::pow(end_node.y - start_node.y, 2));
+
+      // Эквивалентные силы
+      double F1 = df.q * L / 2.0;
+      double F2 = df.q * L / 2.0;
+
+      // Эквивалентные моменты
+      double M1 = df.q * L * L / 12.0;
+      double M2 = -df.q * L * L / 12.0;
+
+      // Добавление в вектор нагрузок
+      load_vector[3 * element.start_node_id + 1] += F1;
+      load_vector[3 * element.end_node_id + 1] += F2;
+
+      load_vector[3 * element.start_node_id + 2] += M1;
+      load_vector[3 * element.end_node_id + 2] += M2;
+  }
+
+
+
+  // Заполнение вектора нагрузок
+  for (const auto& force : forces) {
+      int index_x = 3 * force.node_id;       // Сила по x
+      int index_y = 3 * force.node_id + 1;   // Сила по y
+      load_vector[index_x] += force.fx;
+      load_vector[index_y] += force.fy;
+  }
+
+  for (const auto& moment : moments) {
+      int index_moment = 3 * moment.node_id + 2; // Момент
+      load_vector[index_moment] += moment.value;
+  }
+
+  // Заполнение матрицы жесткости
+  for (const auto& element : elements) {
+      int start_index = 3 * element.start_node_id;
+      int end_index = 3 * element.end_node_id;
+
+      // Пример упрощенной жесткости (диагональный элемент)
+      stiffness_matrix[start_index][start_index] += element.stiffness;
+      stiffness_matrix[end_index][end_index] += element.stiffness;
+  }
+
+  // for (const auto& element : elements) {
+
+  //   double total_force = 0.0;
+  //   double total_moment = 0.0;
+
+  //   for (const auto& load : load_vector) {
+
+  //     int index_x = 3 * load.node_id;       // Сила по x
+  //     int index_y = 3 * load.node_id + 1;   // Сила по y
+
+
+  //     total_force += 3 * load_vector[index_y];
+  //     total_moment += load_vector[index_y+1];
+
+  //   }
+
+  //   double right_reaction = total_moment / beam.length;
+  //   double left_reaction = total_force - right_reaction;
+
+  // }
+
+
+  // Решение системы уравнений
+  // (упрощено для примера)
+  std::vector<double> displacements(eq_count, 0.0);
+
+  for (int i = 0; i < eq_count; ++i) {
+      if (stiffness_matrix[i][i] != 0) {
+          displacements[i] = load_vector[i] / stiffness_matrix[i][i];
+      }
+  }
+
+  // Вывод результатов
+  for (int i = 0; i < n; ++i) {
+      std::cout << "Node " << i << ":\n";
+      std::cout << "  Displacement X: " << displacements[3 * i] << "\n";
+      std::cout << "  Displacement Y: " << displacements[3 * i + 1] << "\n";
+      std::cout << "  Rotation (Moment): " << displacements[3 * i + 2] << "\n";
+  }
+}
