f5dc7128befa1a08099c107d8df8c889f418eeb1
Danya Novikov <dnovikov@softdev.spb.ru>
2025-03-30 10:03:45 +0300

    Remove interface.py; add name field to model

--- python/model.py
+++ python/model.py
@@ -12,6 +12,8 @@ class Model:
         self.y_rot = 0
         self.z_rot = 0
         
+        self.scale = 1
+        
         self.data = ""
         self.nodes = []
         self.elements = []
@@ -19,12 +21,17 @@ class Model:
         self.forces = []
         self.boundary_conditions = []
 
+        self.name = None
+        
         self.draw_node_id = dpg.generate_uuid()
 
         self.model_matrix = (
-            dpg.create_rotation_matrix(math.radians(self.x_rot), [1, 0, 0])
+            
+            dpg.create_translation_matrix([1900//2, 900//2])
+            * dpg.create_rotation_matrix(math.radians(self.x_rot), [1, 0, 0])
             * dpg.create_rotation_matrix(math.radians(self.y_rot), [0, 1, 0])
             * dpg.create_rotation_matrix(math.radians(self.z_rot), [0, 0, 1])
+            * dpg.create_scale_matrix([self.scale, self.scale, self.scale]) 
         )
 
     def rotate_x(self, angle):
@@ -36,12 +43,14 @@ class Model:
     def rotate_z(self, angle):
         self.z_rot += angle
 
-    def load_model(self, filename):
+    def load_model(self, filename:str):
+        self.name = filename.split("/")[-1][:-4]
         with open(filename, "r", encoding="utf-8-sig") as file:
             self.data = json.load(file)
         self.__parse_model()
 
     def __parse_model(self):
+        # TODO Надо бы переписать передачу имени файла
 
         for node in self.data.get("nodes"):
             self.nodes.append(Node(node.get("id"), node.get("coordinates")))
@@ -76,12 +85,23 @@ class Model:
 
     def update(self):
         self.model_matrix = (
-            dpg.create_rotation_matrix(math.radians(self.x_rot), [1, 0, 0])
+            
+            dpg.create_translation_matrix([1900//2, 900//2])
+            * dpg.create_rotation_matrix(math.radians(self.x_rot), [1, 0, 0])
             * dpg.create_rotation_matrix(math.radians(self.y_rot), [0, 1, 0])
             * dpg.create_rotation_matrix(math.radians(self.z_rot), [0, 0, 1])
-            * dpg.create_scale_matrix([0.01, 0.01, 0.01])
+            * dpg.create_scale_matrix([self.scale, self.scale, self.scale]) 
         )
-        print(self.model_matrix)
         dpg.apply_transform(
             self.draw_node_id,  self.model_matrix
         )
+
+
+    def get_model_matrix(self):
+        return self.model_matrix
+    
+    def set_scale(self, scale:float):
+        self.scale = scale
+        
+    def get_scale(self):
+        return self.scale
