d5a52132aabbcd668f23f2a314a4c08b2cf1b483
Danya Novikov <dnovikov@softdev.spb.ru>
2025-04-05 15:58:34 +0300

    Add Geometry Primitives: Vector and Point

---
+++ python/Geometry/Point.py
@@ -0,0 +1,16 @@
+
+class Point:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+        
+    def __add__(self, other):
+        tmp = Point(self.x, self.y, self.z)
+        tmp.x += other.x
+        tmp.y += other.y
+        tmp.z += other.z
+        return tmp
+    
+    def asList(self):
+        return [self.x, self.y, self.z]
