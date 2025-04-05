fe76acfaeb761de81ba9d279076ef308ad0dec0a
Danya Novikov <dnovikov@softdev.spb.ru>
2025-04-05 15:35:24 +0300

    Little refactoring

--- python/force.py
+++
@@ -1,8 +0,0 @@
-﻿import numpy as np
-
-
-class Force:
-    def __init__(self, point, direction):
-        self.point = point
-        self.direction = direction
-        self.force = np.linalg.norm(self.direction)
