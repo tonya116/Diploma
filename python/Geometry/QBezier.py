380116d150f1d8212349510ef870e19445d3462c
Danya Novikov <dnovikov@softdev.spb.ru>
2025-04-05 23:39:03 +0300

    Add quadratic bezier as primitives

---
+++ python/Geometry/QBezier.py
@@ -0,0 +1,8 @@
+class QBezier:
+    def __init__(self, p1, p2, p3, radius, color, thickness):
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
+        self.radius = radius
+        self.color = color
+        self.thickness = thickness
